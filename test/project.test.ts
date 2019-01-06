6972e8c58672147089a77a69dac6c28fdab9902e
Yurick <yurick.hauschild@gmail.com>
2019-01-06 12:05:17 -0200

    🚨  Add test to project

---
+++ test/project.test.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+import * as nock from 'nock'
+
+import Project from '../src/project'
+
+describe('Project', () => {
+  const projects = [
+    {
+      id: 1,
+      name: 'My fake project',
+    },
+  ]
+
+  beforeEach(() => {
+    nock('https://api.getlabor.com.br')
+      .get('/projects?')
+      .reply(200, projects)
+  })
+
+  it('returns all projects from remote', async () => {
+    expect(await Project.all()).to.deep.equal(projects)
+  })
+})
